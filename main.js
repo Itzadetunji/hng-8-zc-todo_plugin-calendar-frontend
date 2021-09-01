8fa2fd0a3d4b217cd8b37fd83c2039eda2a71124
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-09-01 16:52:23 +0100

    main.js

--- main.js
+++ main.js
@@ -20,3 +20,7 @@ document.querySelector('.date h1').innerHTML = months[date.getMonth()];
 document.querySelector('.date p').innerHTML = date.toDateString();
 
 let days = "";
+
+for(let i = 1; i <= 31; i++){
+    days += `<div>${i}</div>`
+}
