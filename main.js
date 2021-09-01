37c8228c20e54ea676333d276abfd2038d7c1c52
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-09-01 17:01:42 +0100

    main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const date = new Date();
 
 date.setDate(1)
 
+console.log(date.getDay())
+
 const monthDays = document.querySelector('.days');
 
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-
-console.log(lastDay);
 
 const months = [
     "January",
