ebd1a38dd7afbd150493c90f08bd888f17b71796
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-09-01 16:48:12 +0100

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 const date = new Date();
 
-const months
+const months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+document.querySelector('.date h1').innerHTML = months[4]
